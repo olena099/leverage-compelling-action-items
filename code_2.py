641df0501b023d971089514b0df7a84f73e92edf
olena099 <142909583+olena099@users.noreply.github.com>
2023-09-24 06:08:05 -0700

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def main():
+def generate_random_data():
+    main()
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return data
+    data = generate_random_data()
+
+    main()
+
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+def generate_random_data():
+    data = generate_random_data()
+    for item in data:
